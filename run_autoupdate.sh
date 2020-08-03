6516229d41e3ccc5dd475ca42f66f08f35c01d0f
Simon Bray <32272674+simonbray@users.noreply.github.com>
2020-08-03 15:51:22 +0200

    Update run_autoupdate.sh

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -3,7 +3,7 @@ virtualenv .au
 source .au/bin/activate
 pip install wheel
 pip install https://github.com/simonbray/planemo/archive/autoupdate-sb.zip  # install from my fork for now
-planemo --help
+.au/bin/planemo --help
 
 # start updating bgruening repo
 git clone https://github.com/bgruening/galaxytools.git
