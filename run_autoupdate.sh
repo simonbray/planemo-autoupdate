2c949d75335f75b96eba9badfcaa0c78d4e69eac
Simon Bray <32272674+simonbray@users.noreply.github.com>
2020-08-03 15:32:03 +0200

    Update run_autoupdate.sh

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -1,1 +1,10 @@
 echo "Starting autoupdate..."
+virtualenv .au
+source .au/bin/activate
+pip install https://github.com/simonbray/planemo/archive/autoupdate-sb.zip  # install from my fork for now
+
+# start updating bgruening repo
+git clone https://github.com/bgruening/galaxytools.git
+cd galaxytools
+planemo autoupdate -r
+git diff
