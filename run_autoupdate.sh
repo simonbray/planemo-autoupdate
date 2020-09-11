fbb33e9201f59591b9c500f20e5b6d1d05ef5a5f
lorrainealisha75 <lorrainealisha75@gmail.com>
2020-09-11 17:48:49 +0200

    removing tabs from git credentials in .planemo.yml

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -13,13 +13,13 @@ git config --global user.email "lorrainealisha75@gmail.com"
 git config --global user.name "lorrainealisha75"
 
 echo "github:" > $HOME/.planemo.yml
-echo "	username: lorrainealisha75" >> $HOME/.planemo.yml
-echo "	password: 6a4aaf73fa0e937de06b35d92810f7d7b64d3b25" >> $HOME/.planemo.yml
+echo "username: lorrainealisha75" >> $HOME/.planemo.yml
+echo "password: 6a4aaf73fa0e937de06b35d92810f7d7b64d3b25" >> $HOME/.planemo.yml
 
 planemo clone https://github.com/lorrainealisha75/planemo.git
 cd planemo/
 git branch
-git checkout -b planemo-test
+git checkout -b planemo-pr
 git commit --allow-empty -m 'new test commit from planemo'
-git push -u origin planemo-test
+git push -u origin planemo-pr
 planemo pull_request -m "planemo test pr"
