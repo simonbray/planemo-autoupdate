56b565ee5c6651f2c3295c3e2104509be66fd5c8
lorrainealisha75 <lorrainealisha75@gmail.com>
2020-09-15 16:14:15 +0200

    Moving the gpg and decrypt-credentials.sh file out of github workflows folder

--- .github/workflows/decrypt_credentials.sh
+++
@@ -1,4 +0,0 @@
-# decrypt the git credentials file
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase "$PLANEMO_AUTOUPDATE_PASSPHRASE" --output $HOME/.planemo.yml .github/workflows/autoupdate-credentials.gpg
---
+++ decrypt_credentials.sh
@@ -0,0 +1,4 @@
+# decrypt the git credentials file
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase "$PLANEMO_AUTOUPDATE_PASSPHRASE" --output $HOME/.planemo.yml autoupdate-credentials.gpg
