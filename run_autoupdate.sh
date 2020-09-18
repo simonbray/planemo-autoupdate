07fa70921317cefc0c933c9ae83b85b665c48844
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-09-18 15:49:43 +0100

    Test

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -9,10 +9,8 @@ pip install -e planemo/
 planemo --help
 
 # git credentials
-git config --global hub.protocol ssh
 git config --global user.email "lorrainealisha75@gmail.com"
 git config --global user.name "Lorraine Coelho"
-export GITHUB_TOKEN="$GITHUB_TOKEN"
 
 rm -rf galaxytools
 planemo clone --skip_fork --branch planemo-pr-`date +%F-%H-%m` https://github.com/lorrainealisha75/galaxytools
