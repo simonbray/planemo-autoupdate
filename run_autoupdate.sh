ff12c8d439b49e8682527a3be2cdda3fce2cc8ac
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-10-01 14:59:59 +0200

    Test

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -4,6 +4,11 @@ sudo pip3 install virtualenv
 virtualenv .au
 source .au/bin/activate
 
+#Install latest hub
+echo "Install hub"
+brew install hub
+echo "hub install successful"
+
 # pip install conda
 pip install https://github.com/simonbray/planemo/archive/autoupdate-sb.zip  # install from my fork for now
 pip install -e planemo/
@@ -14,11 +19,8 @@ git config --global user.email "lorrainealisha75@gmail.com"
 git config --global user.name "Lorraine Coelho"
 
 rm -rf galaxytools
-planemo clone --branch planemo-pr-13-`date +%F-%H-%m` git@github.com:lorrainealisha75/galaxytools
+planemo clone --skip_fork --branch planemo-pr-93-`date +%F-%H-%m` git@github.com:lorrainealisha75/galaxytools
 echo "Cloning successful"
-echo "Install hub"
-brew install hub
-echo "hub install successful"
 cd galaxytools/
 echo "Folder is:"
 pwd
@@ -27,6 +29,6 @@ git commit --allow-empty -m 'new test commit from planemo'
 echo "Git pull origin master"
 git pull origin master
 echo "Push"
-git push -u origin planemo-pr-13-`date +%F-%H-%m`
+git push -u origin planemo-pr-93-`date +%F-%H-%m`
 echo "Create pull request"
 planemo pull_request -m "planemo test pr 11"
