adc731367fd645813d1dbac1822924c6a2cf0559
Lorraine Coelho <lorrainealisha75@gmail.com>
2020-10-01 13:37:41 +0200

    Test

--- run_autoupdate.sh
+++ run_autoupdate.sh
@@ -3,6 +3,7 @@ pip3 install wheel
 sudo pip3 install virtualenv
 virtualenv .au
 source .au/bin/activate
+
 # pip install conda
 pip install https://github.com/simonbray/planemo/archive/autoupdate-sb.zip  # install from my fork for now
 pip install -e planemo/
@@ -13,13 +14,13 @@ git config --global user.email "lorrainealisha75@gmail.com"
 git config --global user.name "Lorraine Coelho"
 
 rm -rf galaxytools
-planemo clone --skip_fork --branch planemo-11pr-`date +%F-%H-%m` git@github.com:lorrainealisha75/galaxytools
+planemo clone --skip_fork --branch planemo-pr-`date +%F-%H-%m` git@github.com:lorrainealisha75/galaxytools
 echo "Cloning successful"
 cd galaxytools/
-echo "Folder:"
+echo "Folder is:"
 pwd
 echo "Commit"
 git commit --allow-empty -m 'new test commit from planemo'
 echo "Push"
-git push -u origin planemo-11pr-`date +%F-%H-%m`
+git push -u origin planemo-pr-`date +%F-%H-%m`
 planemo pull_request -m "planemo test pr 11"
